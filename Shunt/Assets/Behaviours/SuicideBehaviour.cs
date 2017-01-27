ce8eee108de14f3746e7233b0c424d207bd0e17c
Roger Creyke <creyke@gmail.com>
2017-01-27 20:18:19 +0000

    Renamed SuicideBehaviourScript to SuicideBehaviour.

--- Shunt/Assets/Behaviours/SuicideBehaviour.cs
+++ Shunt/Assets/Behaviours/SuicideBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SuicideBehaviourScript : MonoBehaviour {
+public class SuicideBehaviour : MonoBehaviour {
 
     public float delay = 1;
 
