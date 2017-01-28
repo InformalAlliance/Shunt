b279127ab094e777b84f0e0e604705a3b1fc8d86
Roger Creyke <creyke@gmail.com>
2017-01-28 21:41:16 +0000

    Inserter AdvancedBezierCurve abstraction between third party BezierCurve and TrackPiece.

--- Shunt/Assets/Entites/Track/TrackPiece.cs
+++ Shunt/Assets/Entites/Track/TrackPiece.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.Splines;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,7 @@ namespace Assets.Entites.Track
 {
     [ExecuteInEditMode]
     [Serializable]
-    public class TrackPiece : BezierCurve
+    public class TrackPiece : AdvancedBezierCurve
     {
         public List<TrackPiece> forwardEdges;
         public List<TrackPiece> reverseEdges;
---
+++ Shunt/Assets/Splines/AdvancedBezierCurve.cs
@@ -0,0 +1,6 @@
+﻿namespace Assets.Splines
+{
+    public class AdvancedBezierCurve : BezierCurve
+    {
+    }
+}
